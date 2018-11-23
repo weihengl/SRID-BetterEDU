0de016e5dcda9bfa511e4c0c185edabf59f2abd4
XiaoyuCong <xiaoyuco@andrew.cmu.edu>
2018-11-22 16:22:18 -0800

    gallery

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var galleryRouter = require('./routes/gallery');
 var WebSocket = require('ws');
 
 var app = express();
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/gallery',galleryRouter);
 
 
 // catch 404 and forward to error handler
