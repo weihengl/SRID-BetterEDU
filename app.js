3df0fa403d5870cf276797137f536cb7cef40c4e
Steven Tang <767527493@qq.com>
2018-11-25 03:14:26 -0800

    Add three pages related to video conforence

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/videoConference.js
+++ routes/videoConference.js
@@ -1,8 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    res.render('videoConference');
+});
 
 router.get('/detail',(req,res)=>{
     res.render('videoConferenceDetails');
+});
+
+router.get('/join',(req,res)=>{
+    res.render('joinVideoConference');
+});
+
+router.get('/setting',(req,res)=>{
+    res.render('videoConferenceSetting');
 });
 
 module.exports = router;
