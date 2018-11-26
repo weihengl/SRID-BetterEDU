48164fe13129d335c495083f97728bf52131b172
weihengl <weihengl@andrew.cmu.edu>
2018-11-26 08:25:34 -0800

    Add login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.use('/game',require("./game"));
-
+router.use('/login',require('./login'));
 router.get('/', (req, res) => {
 	res.render('index');
 });
