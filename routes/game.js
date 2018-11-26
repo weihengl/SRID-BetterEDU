6ca3d15b5b96b3e31ff24fdd49818d51fbb67c4f
weihengl <weihengl@andrew.cmu.edu>
2018-11-26 14:10:05 -0800

    no message

--- routes/game.js
+++ routes/game.js
@@ -4,7 +4,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(8080);
 
 io.sockets.on('connection', (socket) => {
 
